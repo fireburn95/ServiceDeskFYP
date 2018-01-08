bf28f5b45b92eb636924a8f90fe1884ff55bc683
Abbas <fireburn195@gmail.com>
2018-01-08 13:21:14 +0000

    Add project files.

---
+++ ServiceDeskFYP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceDeskFYP.Startup))]
+namespace ServiceDeskFYP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
