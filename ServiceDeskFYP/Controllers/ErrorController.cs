f0153f30b6c048780b0c887332829111c958ef93
Abbas Pardhan <P13223165@my365.dmu.ac.uk>
2018-03-19 16:35:53 +0000

    System - Handle error 400 and 404

---
+++ ServiceDeskFYP/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using ServiceDeskFYP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceDeskFYP.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult NotFound(string aspxerrorpath = null)
+        {
+            Helpers.LogEvent("Error", "The user navigated to a page which cannot be found (" + aspxerrorpath + ")");
+            return View();
+        }
+
+        public ActionResult Error400(string aspxerrorpath = null)
+        {
+            Helpers.LogEvent("Error", "A Bad Request error has occured with the user");
+            return View();
+        }
+    }
+}
